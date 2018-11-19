3b8b02c0f446c315fd032fde1ec1921f5d945105
Kevin Heinicke <kevin@kehei.de>
2018-11-19 17:52:00 +0100

    Disentangle loading from prediction

--- clustering.py
+++ clustering.py
@@ -2,13 +2,20 @@ from load_data import load_data
 import numpy as np
 from sklearn.cluster import DBSCAN
 
+
+def predict_cluster(df, epsilon=20):
+    cluster_finder = DBSCAN(eps=epsilon)
+
+    df["rho"] = np.sqrt(df.x**2 + df.y**2)
+    df["cluster_idx"] = cluster_finder.fit_predict(df[["rho", "z"]])
+
+    return df
+
+
 def clustering(filename, epsilon=20):
     d = load_data(filename).dropna()
 
-    cluster_finder = DBSCAN(eps=epsilon)
-
-    d["rho"] = np.sqrt(d.x**2 + d.y**2)
-    d["cluster_idx"] = cluster_finder.fit_predict(d[["rho", "z"]])
+    d = predict_cluster(d)
 
     means = list()
 
@@ -16,10 +23,10 @@ def clustering(filename, epsilon=20):
         # skip tracks belonging to no clusters
         if v==-1:
             continue
-    
+
         x_mean = d.query("cluster_idx=={}".format(v)).x.mean()
         y_mean = d.query("cluster_idx=={}".format(v)).y.mean()
         z_mean = d.query("cluster_idx=={}".format(v)).z.mean()
         means.append((x_mean, y_mean, z_mean))
-    
+
     return means
